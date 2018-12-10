e1c64c22a6f3513ec7651723a24fcc36f33dfc75
Niklas Lavrell <niklas.lavrell@gmail.com>
2018-12-10 23:38:45 +0800

    updated content max width

--- src/components/layout.js
+++ src/components/layout.js
@@ -116,7 +116,7 @@ const Layout = ({ children }) => (
           <div
             style={{
               margin: '0 auto',
-              maxWidth: 960,
+              maxWidth: '33rem',
               padding: '0px 1.0875rem 1.45rem',
               paddingTop: 0,
               flexGrow: 1,
