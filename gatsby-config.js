9259c6c07d446372ac2169d84bc0c62fac4a0508
Niklas Lavrell <niklas.lavrell@gmail.com>
2018-12-10 15:27:18 +0800

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,12 @@ module.exports = {
         icon: 'src/images/wizardicon512.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-117499442-2',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
