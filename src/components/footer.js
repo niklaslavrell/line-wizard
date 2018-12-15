46b3ce76dbb7acaae6c0f80d64d44df6c9b91afb
Niklas Lavrell <niklas.lavrell@gmail.com>
2018-12-15 21:29:03 +0800

    added an about page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import Coffee from '../components/coffee'
 
@@ -9,6 +10,34 @@ const Footer = () => (
       fontSize: '0.9rem',
     }}
   >
+    <div
+      style={{
+        padding: '0.5rem',
+        paddingBottom: '0rem',
+        textAlign: 'center',
+        color: 'white',
+      }}
+    >
+      <Link
+        to="/about"
+        style={{
+          color: 'white',
+          marginRight: '0.5rem',
+        }}
+      >
+        <strong>About</strong>
+      </Link>
+      {'|'}
+      <Link
+        to="/help"
+        style={{
+          color: 'white',
+          marginLeft: '0.5rem',
+        }}
+      >
+        <strong>Help</strong>
+      </Link>
+    </div>
     <div
       style={{
         margin: '0 auto',
---
+++ src/pages/about.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Button from '../components/button'
+import Emoji from '../components/emoji'
+import Layout from '../components/layout'
+import niklasAndHanna from '../images/niklas-and-hanna.jpg'
+
+const AboutPage = () => (
+  <Layout>
+    <h1>About</h1>
+    <p>
+      <strong>
+        <Emoji symbol="👋" />
+        Hi
+      </strong>
+      <br />
+      We are a nomad couple travelling and building products we hope people will
+      love <Emoji symbol="💚" />
+    </p>
+    <img src={niklasAndHanna} alt="Niklas and Hanna" />
+    <p>
+      You can find us at <a href="https://www.lavrell.se">Niklas website</a> and{' '}
+      <a href="https://www.hannasoderquist.se">Hannas website</a>. Here are some
+      other products that we are making at the moment:
+    </p>
+    <p>
+      <Emoji symbol="💁‍" />{' '}
+      <a href="https://www.letsborrowit.com">
+        <strong>Let's Borrowit</strong>
+      </a>{' '}
+      - make people consume less and borrow more
+    </p>
+    <p>
+      <Emoji symbol="🏡" />{' '}
+      <a href="https://www.botillsammans.nu">
+        <strong>Bo Tillsammans</strong>
+      </a>{' '}
+      - find roommates, live together
+    </p>
+    <p>
+      <Emoji symbol="🤖" />{' '}
+      <a href="https://www.chekr.app">
+        <strong>Chekr</strong>
+      </a>{' '}
+      - bot that watch our stocks, so we can spend time on other stuff
+    </p>
+    <p>
+      <Emoji symbol="🚀" />{' '}
+      <a href="https://www.swedishtechmakers.com">
+        <strong>Swedish Tech Makers</strong>
+      </a>{' '}
+      - community with people that want to ship
+    </p>
+    <p>
+      <Emoji symbol="🌱" />{' '}
+      <a href="https://www.thehappylist.io">
+        <strong>The Happy List</strong>
+      </a>{' '}
+      - articles and talks that helps us grow
+    </p>
+    <Link to="/">
+      <Button>Cool, back to the Line Wizard</Button>
+    </Link>
+  </Layout>
+)
+
+export default AboutPage
